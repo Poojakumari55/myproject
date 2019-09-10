3be7a87e47b99d704ca663e33ceabd3d882a3962
Poojakumari55 <46664671+Poojakumari55@users.noreply.github.com>
2019-09-10 12:39:06 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner
+public class test
+{
+public void add(int a, int b)
+{
+int sum;
+sum = a+b; 
+}
+public static void main(string[] args)
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+system.out.println("enter first number");
+a=sc.nextInt();
+system.out.println("enter second number");
+b=sc.nextInt();
+test obj = new test();
+obj.add(a,b);
+system.out.println("sum=" + sum);
+}
+}
